c68ecfcd637ad59cbbf7bda7422b213a8baf18a7
Salman Hagwah <salman.rus20@gmail.com>
2021-12-04 21:05:00 +0300

    не все

---
+++ task 6/1.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.list');
+const clear = document.querySelector('.clear');
+
+
+btn.addEventListener('click', function(event) {
+    let li = document.createElement('li');
+    li.innerHTML = input.value;
+    // li.className = "red";
+    list.appendChild(li);
+
+    if (li.innerHTML > 10) {
+        li.className = "red";
+    }
+    if (li.innerHTML < 10) {
+
+        li.className = "green";
+    }
+    if (li.innerHTML == 10) {
+        li.className = "blue";
+    }
+
+});
